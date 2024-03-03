33ffd539a92639590c07f7a2de2fc3c20c120969
One <imone@tuta.io>
2024-03-03 16:49:43 +0800

    fix args order in header (WIP)

--- csrc/bf16_fused_adam.h
+++ csrc/bf16_fused_adam.h
@@ -5,10 +5,10 @@ namespace bf16_fused_adam {
 
 void bf16_fused_adamw_cuda_impl_(
     at::TensorList params,
+    at::TensorList mantissas,
     at::TensorList grads,
     at::TensorList exp_avgs,
     at::TensorList exp_avg_sqs,
-    at::TensorList mantissas,
     at::TensorList state_steps,
     const double lr,
     const double beta1,
